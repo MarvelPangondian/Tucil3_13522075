a419818aa43c320328a19f7cc47a4c5718dc14e1
Marvel Pangondian <marvel.pangondian01@gmail.com>
2024-05-06 03:21:08 +0700

    feat: modify bat file

--- run.bat
+++ run.bat
@@ -1,6 +1,10 @@
 @echo off
 setlocal
 javac -d bin src\main\Main.java src\main\datastructure\Node.java src\main\datastructure\PriorityQueueNode.java src\main\dictionary\Dictionary.java src\main\algorithm\UCS.java src\main\customexception\CustomException.java src\main\algorithm\AStar.java src\main\algorithm\GreedyBestFirst.java src\main\customexception\InvalidStartWordException.java src\main\customexception\InvalidEndWordException.java src\main\customexception\InvalidLengthWord.java src\main\util\StringUtil.java src\main\algorithm\SearchAlgorithm.java src\main\cli\CLI.java src\main\gui\AppGUI.java src\main\customexception\EmptyWordException.java src\main\prompt\Prompt.java
+if %errorlevel% neq 0 (
+    echo Something went wrong.
+    exit /b
+)
 java -cp bin Main
 del /s /q bin\*.class
 endlocal
